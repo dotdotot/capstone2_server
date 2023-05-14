c0bacaeb3b0756e8c6c476b7010ae3d742e73d4f
dotdotot@jiran.com <dotdotot@jiran.com>
2023-05-14 21:52:15 +0900

    feat/club 동아리
    - club 모델 생성
    - 마이그레이션 파일에 clubs추가
    - user 경고 횟수 칼럼 추가

---
+++ app/Models/Club.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Jenssegers\Mongodb\Eloquent\HybridRelations;
+
+/**
+ * public @method departments()
+ */
+class Club extends BaseModel
+{
+    use HybridRelations;
+    use SoftDeletes;
+
+    protected $connection = 'pgsql';
+    protected $table = 'clubs';
+
+    protected $dates = [
+        'deleted_at'
+    ];
+
+    protected $fillable = [
+        'name', 'code', 'grade', 'created_at', 'updated_at', 'deleted_at'
+    ];
+
+    public function __construct(array $attributes = array())
+    {
+        parent::__construct($attributes);
+
+        # 클럽이름, 클럽 코드
+        $this->name = isset($attributes['name']) ? $attributes['name'] : '';
+        $this->code = isset($attributes['code']) ? $attributes['code'] : '';
+
+        # 추후 수익 모델을 위한 칼럼
+        $this->grade = isset($attributes['grade']) ? $attributes['grade'] : 'normal';
+
+        // 삭제/추가/수정 시간
+        $this->created_at = isset($attributes['created_at']) ? $attributes['created_at'] : Carbon::now();
+        $this->updated_at = isset($attributes['updated_at']) ? $attributes['updated_at'] : Carbon::now();
+        $this->deleted_at = isset($attributes['deleted_at']) ? $attributes['deleted_at'] : null;
+    }
+}
