7ba4177c6a7fe22ff486dd0468798471db4dd9a1
dotdotot@jiran.com <dotdotot@jiran.com>
2023-05-16 12:02:53 +0900

    Feat/departmentSeeder 학과시더
    - 학과시더 생성 및 작성

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Club;
+use App\Models\Department;
+
 class DepartmentSeeder extends Seeder
 {
     /**
@@ -14,6 +17,12 @@ class DepartmentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $club = Club::where('name', 'C403')->select('id', 'name')->first();
+        Department::create([
+            'club_id' => $club->id,
+            'name' => '컴퓨터공학과',
+            'code' => Club::clubCodeCreate(),
+            'position' => Department::where('club_id', $club->id)->count(),
+        ]);
     }
 }
