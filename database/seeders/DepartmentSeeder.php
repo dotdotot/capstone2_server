9504910b862a57a7d87a8160d8725443e3a4b5fb
dotdotot@jiran.com <dotdotot@jiran.com>
2023-05-26 17:48:35 +0900

    Feat/departmentSeeder 부서 시더 생성

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Seeder;
 use App\Models\Club;
 use App\Models\Department;
 
+use Faker\Factory as Faker;
+
 class DepartmentSeeder extends Seeder
 {
     /**
@@ -18,11 +20,29 @@ class DepartmentSeeder extends Seeder
     public function run()
     {
         $club = Club::where('name', 'C403')->select('id', 'name')->first();
+        $faker = Faker::create('ko_KR');
+
         Department::create([
             'club_id' => $club->id,
             'name' => '컴퓨터공학과',
-            'code' => Club::clubCodeCreate(),
+            'code' => Department::departmentCodeCreate(),
             'position' => Department::where('club_id', $club->id)->count(),
         ]);
+
+        # faker를 사용하여 10개의 학과 생성
+        for($i = 0; $i < 10; $i++) {
+            $departmentName = $faker->numerify('테스트학과 ##');
+            if(Department::where('name', $departmentName)->first() !== null) {
+                $i--;
+                continue;
+            }
+
+            Department::create([
+                'club_id' => $club->id,
+                'name' => $departmentName,
+                'code' => Department::departmentCodeCreate(),
+                'position' => Department::where('club_id', $club->id)->count(),
+            ]);
+        }
     }
 }
