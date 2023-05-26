878b61d60796c0acbd99fe2d942a708b3cb47e64
dotdotot@jiran.com <dotdotot@jiran.com>
2023-05-26 17:47:14 +0900

    Feat/proejctConsent 프로젝트 참가여부
    - 모델 및 시더 생성

---
+++ app/Models/ProjectConsent.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Jenssegers\Mongodb\Eloquent\SoftDeletes;
+use Carbon\Carbon;
+
+class ProjectConsent extends BaseMongoModel
+{
+    use SoftDeletes;
+
+    protected $connection = 'mongodb';
+    protected $table = 'project_consents';
+
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = [
+        'club_id', 'user_id', 'consent'
+    ];
+
+    public function __construct(array $attributes = array())
+    {
+        parent::__construct($attributes);
+
+        $this->club_id = isset($attributes['company_id']) ? $attributes['company_id'] : null;
+        $this->user_id = isset($attributes['user_id']) ? $attributes['user_id'] : null;
+        $this->consent = isset($attributes['consent']) ? $attributes['consent'] : null;
+
+        // 삭제/추가/수정 시간
+        $this->deleted_at = isset($attributes['deleted_at']) ? $attributes['deleted_at'] : null;
+        $this->updated_at = isset($attributes['updated_at']) ? $attributes['updated_at'] : Carbon::now();
+        $this->created_at = isset($attributes['created_at']) ? $attributes['created_at'] : Carbon::now();
+    }
+}
---
+++ database/seeders/ProjectConsentSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\ProjectConsent;
+use App\Models\Club;
+use App\Models\User;
+
+class ProjectConsentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $club = Club::where('id', 1)->first();
+        User::where('club_id', $club->id)->each(function ($user) use ($club) {
+            $projectConsent = new ProjectConsent();
+            $projectConsent->club_id = $club->id;
+            $projectConsent->user_id = $user->id;
+            $projectConsent->consent = true;
+            $projectConsent->save();
+        });
+    }
+}
