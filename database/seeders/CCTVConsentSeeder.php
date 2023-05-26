03d24e5159c44c5c16589282767e97d54abf29e1
dotdotot@jiran.com <dotdotot@jiran.com>
2023-05-26 17:46:00 +0900

    Feat/CCTVConsent cctv기록
    - cctv 기록 몽고 모델 생성
    - cctv 시더 및 마이그레이션 정의

---
+++ database/seeders/CCTVConsentSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Club;
+use App\Models\User;
+use App\Models\CCTVConsent;
+
+class CCTVConsentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $club = Club::where('id', 1)->first();
+        User::where('club_id', $club->id)->each(function ($user) use ($club) {
+            $cctvConsent = new CCTVConsent();
+            $cctvConsent->club_id = $club->id;
+            $cctvConsent->user_id = $user->id;
+            $cctvConsent->consent = true;
+            $cctvConsent->save();
+        });
+    }
+}
