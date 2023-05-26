d0dc7db91d03f12e270a695fae9b6ce778c17247
dotdotot@jiran.com <dotdotot@jiran.com>
2023-05-26 17:48:16 +0900

    Feat/rankPermission 랭크 권한
    - 사용자별 권한 부여
    - 시더 생성

---
+++ app/Models/RankPermission.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Jenssegers\Mongodb\Eloquent\HybridRelations;
+
+/**
+ * public @method departments()
+ */
+class RankPermission extends BaseModel
+{
+    use HybridRelations;
+    use SoftDeletes;
+
+    protected $connection = 'pgsql';
+    protected $table = 'rank_permissions';
+
+    protected $dates = [
+        'deleted_at'
+    ];
+
+    protected $fillable = [
+        'club_id', 'rank_id', 'board_access', 'comment_access', 'image_add_access', 'anonymous_comment_access', 'community_add_access', 'user_ben_access', 'admin_board_access', 'user_change_access', 'admin_access', 'created_at', 'updated_at', 'deleted_at'
+    ];
+
+    public function __construct(array $attributes = array())
+    {
+        parent::__construct($attributes);
+
+        # 동아리 아이디
+        $this->club_id = isset($attributes['club_id']) ? $attributes['club_id'] : null;
+        # 랭크 아이디
+        $this->rank_id = isset($attributes['rank_id']) ? $attributes['rank_id'] : null;
+        # 게시판 권한
+        $this->board_access = isset($attributes['board_access']) ? $attributes['board_access'] : false;
+        # 댓글 권한
+        $this->comment_access = isset($attributes['comment_access']) ? $attributes['comment_access'] : null;
+        # 이미지 업로드 권한
+        $this->image_add_access = isset($attributes['image_add_access']) ? $attributes['image_add_access'] : null;
+        # 익명 댓글 권한
+        $this->anonymous_comment_access = isset($attributes['anonymous_comment_access']) ? $attributes['anonymous_comment_access'] : null;
+
+        # 커뮤니티 추가 권한
+        $this->community_add_access = isset($attributes['community_add_access']) ? $attributes['community_add_access'] : null;
+        # 사용자 벤 권한
+        $this->user_ben_access = isset($attributes['user_ben_access']) ? $attributes['user_ben_access'] : null;
+        # 관리자 관련 게시판 탭 권한
+        $this->admin_board_access = isset($attributes['admin_board_access']) ? $attributes['admin_board_access'] : null;
+        # 특정 사용자 변경 권한
+        $this->user_change_access = isset($attributes['user_change_access']) ? $attributes['user_change_access'] : null;
+        # 어드민 권한
+        $this->admin_access = isset($attributes['admin_access']) ? $attributes['admin_access'] : null;
+
+        $this->position = isset($attributes['position']) ? $attributes['position'] : 0;
+
+        // 삭제/추가/수정 시간
+        $this->created_at = isset($attributes['created_at']) ? $attributes['created_at'] : Carbon::now();
+        $this->updated_at = isset($attributes['updated_at']) ? $attributes['updated_at'] : Carbon::now();
+        $this->deleted_at = isset($attributes['deleted_at']) ? $attributes['deleted_at'] : null;
+    }
+
+    public function club()
+    {
+        return $this->belongsTo(Club::class);
+    }
+
+    public function rank()
+    {
+        return $this->hasOne(Rank::class);
+    }
+}
---
+++ database/seeders/RankPermissionSeeder.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Club;
+use App\Models\Rank;
+use App\Models\RankPermission;
+
+class RankPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $club = Club::where('name', 'C403')->first();
+        $ranks = Rank::where('club_id', $club->id)->get();
+
+        # 방장 권한
+        $adminUserPermission = new RankPermission();
+        $adminUserPermission->club_id = $club->id;
+        $adminUserPermission->rank_id = $ranks->where('club_id', $club->id)->where('name', '방장')->value('id');
+        $adminUserPermission->board_access = true;
+        $adminUserPermission->comment_access = true;
+        $adminUserPermission->image_add_access = true;
+        $adminUserPermission->anonymous_comment_access = true;
+        $adminUserPermission->community_add_access = true;
+        $adminUserPermission->user_ben_access = true;
+        $adminUserPermission->admin_board_access = true;
+        $adminUserPermission->user_change_access = true;
+        $adminUserPermission->admin_access = true;
+        $adminUserPermission->position = 0;
+        $adminUserPermission->save();
+
+        # 팀장 권한
+        $teamLeaderPermission = new RankPermission();
+        $teamLeaderPermission->club_id = $club->id;
+        $teamLeaderPermission->rank_id = $ranks->where('club_id', $club->id)->where('name', '팀장')->value('id');
+        $teamLeaderPermission->board_access = true;
+        $teamLeaderPermission->comment_access = true;
+        $teamLeaderPermission->image_add_access = true;
+        $teamLeaderPermission->anonymous_comment_access = true;
+        $teamLeaderPermission->community_add_access = true;
+        $teamLeaderPermission->user_ben_access = false;
+        $teamLeaderPermission->admin_board_access = true;
+        $teamLeaderPermission->user_change_access = true;
+        $teamLeaderPermission->admin_access = false;
+        $teamLeaderPermission->position = 1;
+        $teamLeaderPermission->save();
+
+        # 일반 사용자 권한
+        $normalUserPrmission = new RankPermission();
+        $normalUserPrmission->club_id = $club->id;
+        $normalUserPrmission->rank_id = $ranks->where('club_id', $club->id)->where('name', '일반')->value('id');
+        $normalUserPrmission->board_access = true;
+        $normalUserPrmission->comment_access = true;
+        $normalUserPrmission->image_add_access = true;
+        $normalUserPrmission->anonymous_comment_access = true;
+        $normalUserPrmission->community_add_access = false;
+        $normalUserPrmission->user_ben_access = false;
+        $normalUserPrmission->admin_board_access = false;
+        $normalUserPrmission->user_change_access = false;
+        $normalUserPrmission->admin_access = false;
+        $normalUserPrmission->position = 2;
+        $normalUserPrmission->save();
+
+        # 명예 사용자 권한
+        $honorUserPermission = new RankPermission();
+        $honorUserPermission->club_id = $club->id;
+        $honorUserPermission->rank_id = $ranks->where('club_id', $club->id)->where('name', '명예')->value('id');
+        $honorUserPermission->board_access = true;
+        $honorUserPermission->comment_access = true;
+        $honorUserPermission->image_add_access = false;
+        $honorUserPermission->anonymous_comment_access = false;
+        $honorUserPermission->community_add_access = false;
+        $honorUserPermission->user_ben_access = false;
+        $honorUserPermission->admin_board_access = false;
+        $honorUserPermission->user_change_access = false;
+        $honorUserPermission->admin_access = false;
+        $honorUserPermission->position = 3;
+        $honorUserPermission->save();
+    }
+}
